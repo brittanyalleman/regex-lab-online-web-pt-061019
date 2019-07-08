aea70dd25a71363f86f53dd5644f7e67ff415b7a
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-08 22:07:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,7 +6,9 @@ end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^[un\w/{ing$}) ? true : false
+text.scan(UNun\w[/ing$/]) ? true : false
+  [#UNun\w[/ing$/]
+  #(/^[un\w/{ing$})
 end
 
 def words_five_letters_long(text)
