8d79e0364397916d29e6491dcad0e4f9f1863162
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-08 21:28:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,8 @@
 def starts_with_a_vowel?(word)
-starts_with_a_vowel?(word).scan/^[aeiou]/
+starts_with_a_vowel?(word).scan(/^[aeiou]/)
 end
+
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 
